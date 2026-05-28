28004fc484cd7f818d74582e24e333fa72ce95a7
Paweł Łukasz <pawel.lukasz001@gmail.com>
2026-05-28 19:56:57 +0200

    fix audio service activity

--- android/app/src/main/kotlin/com/mobile/pablo/radioflow/MainActivity.kt
+++ android/app/src/main/kotlin/com/mobile/pablo/radioflow/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.mobile.pablo.radioflow
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
